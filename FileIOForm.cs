f7e6f936ae2e5c390bd51a0cf615db07e7992de9
Jonathan A Hand <jhand1@my.westga.edu>
2025-10-15 20:13:40 -0400

    Initial commit

---
+++ FileIOForm.cs
@@ -0,0 +1,17 @@
+namespace JonathanHandA6
+{
+    public partial class FileIOForm : Form
+    {
+        public FileIOForm()
+        {
+            InitializeComponent();
+        }
+
+        private void FileIOForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
